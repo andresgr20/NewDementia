ddd296d3ff71b887f9e86af54524699cb1cd1db0
Aaron Kelly-Barker <aaron.kelly.barker@gmail.com>
2017-03-05 15:07:33 -0500

    Timer tasks exists. Occurs 5 seconds after button press

--- app/src/main/java/com/example/andres/newdementia/PatientMain.java
+++ app/src/main/java/com/example/andres/newdementia/PatientMain.java
@@ -4,32 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Andres on 2017-03-05.
  */
 
 
 public class PatientMain extends Activity {
+
+    private class PatientTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            Intent i = new Intent(PatientMain.this, PatientAct.class);
+            startActivity(i);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patientmain);
     }
     public void onButtonClick(View view){
+        Timer timer = new Timer();
+        PatientTimerTask ptt = new PatientTimerTask();
         if(view.getId() == R.id.Bathe){ // need to add the user name
             Intent i = new Intent(PatientMain.this, PatientAct.class);
             startActivity(i);
+            timer.schedule(ptt, 5000);
         }
         else if(view.getId() == R.id.Cook){
-            Intent i = new Intent(PatientMain.this, PatientAct.class);
-            startActivity(i);
+            timer.schedule(ptt, 5000);
         }
         else if(view.getId() == R.id.Walk){
             Intent i = new Intent(PatientMain.this, PatientAct.class);
             startActivity(i);
+            timer.schedule(ptt, 5000);
         }else if(view.getId() == R.id.Dog){
             Intent i = new Intent(PatientMain.this, PatientAct.class);
             startActivity(i);
+            timer.schedule(ptt, 5000);
         }
     }
 }
